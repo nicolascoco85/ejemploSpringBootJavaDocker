407dbc5b94ff9f4dcbc4972842f90fd63f96aa6d
Nicolas <nicolascoco85@gmail.com>
2023-03-29 14:25:23 -0300

    sE MODIFICA LA LECTURA DE VARIABLES

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,26 +1,30 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import io.github.cdimascio.dotenv.Dotenv;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @SpringBootApplication
+@ConfigurationProperties(prefix = "application.properties")
 public class DemoApplication {
 
-    @RequestMapping("/")
-    String home() {
-    	Dotenv dotenv = Dotenv.load();
-    	String name = dotenv.get("NAME_DB");
-        return "Hello World!" + name;
-    }
+	@Value("${NAME_DB}")
+	String name; // = dotenv.get("NAME_DB");
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	@RequestMapping("/")
+	String home() {
+
+		return "Hello World!" + name;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
 }
