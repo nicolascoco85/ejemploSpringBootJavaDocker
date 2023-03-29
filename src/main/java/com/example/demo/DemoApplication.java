aed5b94d4d8801ce043bb06928e4858f6d09c94d
Nicolas <nicolascoco85@gmail.com>
2023-03-29 12:23:03 -0300

    lectura de variables de entorno

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,9 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+    	Dotenv dotenv = Dotenv.load();
+    	String name = dotenv.get("NAME_DB");
+        return "Hello World!" + name;
     }
 
     public static void main(String[] args) {
